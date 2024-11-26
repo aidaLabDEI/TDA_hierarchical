ad7e22e7644596bca74ef37b3b503c7edaa56340
Boninsegna Fabrizio <fabrizio.boninsegna@phd.unipd.it>
2024-11-26 10:33:35 +0100

    MOD:

--- run_command/Italy.sh
+++ run_command/Italy.sh
@@ -4,5 +4,5 @@ cd ..
 cd experiments || exit
 # run the python program synthetic_dataset.py
 
-python Italy_different_norms.py --delta 1e-8 --show-tqdm --epsilons 1,10 --num-experiments 5 --final-level 6 \
+python Italy_experiments.py --delta 1e-8 --show-tqdm --epsilons 1,10 --num-experiments 5 --final-level 6 \
 --save-path "../results/Italy"
