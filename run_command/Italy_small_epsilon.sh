d0af3769643b30fb1e3415d7dd29833d6f38e415
Boninsegna Fabrizio <fabrizio.boninsegna@phd.unipd.it>
2025-02-05 08:14:02 +0000

    MOD

--- run_command/Italy_small_epsilon.sh
+++ run_command/Italy_small_epsilon.sh
@@ -4,6 +4,6 @@ cd ..
 cd experiments || exit
 # run the python program synthetic_dataset.py
 
-python Italy_experiments.py --delta 1e-8 --show-tqdm --epsilons 0.1 --num-experiments 5 --final-level 6 \
+python Italy_experiments.py --delta 1e-8 --show-tqdm --epsilons 0.1 --num-experiments 1 --final-level 6 \
 --file-path "../data/Italy" \
 --save-path "../results/Italy_small_epsilon"
